d20e5f46ed7086f6dd26359f184896389ba952b4
ammarsjw <ammarsjw@gmail.com>
2025-05-23 19:30:34 +0500

    fix: deployment script

--- scripts/deploy-poseidon-contract.js
+++ scripts/deploy-poseidon-contract.js
@@ -1,58 +1,71 @@
-// License: LGPL-3.0+
+import dotenv from "dotenv";
+dotenv.config();
 
-import ethers from "ethers"
+import ethers from "ethers";
+import fs from "fs";
+import * as envfile from "envfile";
 
-import dotenv from "dotenv"
-dotenv.config()
+import { createCode, generateABI } from "../src/poseidon_gencontract.js";
 
-import fs from "fs"
-import * as envfile from "envfile"
-const parsedENVFile = envfile.parse(fs.readFileSync(".env"))
+const parsedENVFile = envfile.parse(fs.readFileSync(".env"));
 
-import { createCode, generateABI } from "../src/poseidon_gencontract.js"
+const main = async () => {
+    try {
+        // Creating provider.
+        const provider = new ethers.providers.JsonRpcProvider(process.env.URL);
 
-async function main() {
-    // Creating provider and signer.
-    const provider = new ethers.providers.JsonRpcProvider(process.env.URL)
-    const signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
+        // Logging chain data.
+        const chainId = (await provider.getNetwork()).chainId;
+        console.log("Deploying on chain ID:", chainId);
 
-    // Creating PoseidonT3 factory.
-    const poseidonT3Factory = new ethers.ContractFactory(
-        generateABI(2),
-        createCode(2),
-        signer
-    )
+        // Creating signer.
+        const signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
 
-    // Creating PoseidonT4 factory.
-    const poseidonT4Factory = new ethers.ContractFactory(
-        generateABI(3),
-        createCode(3),
-        signer
-    )
+        // Getting gas price and gas limit.
+        const gasPrice = (await provider.getFeeData()).gasPrice;
+        const gasLimit = ethers.BigNumber.from(6000000);
 
-    // Deploying PoseidonT3 library.
-    const poseidonT3 = await poseidonT3Factory.deploy()
-    await poseidonT3.deployed()
-    const poseidonT3Address = poseidonT3.address
-    console.log("PoseidonT3 deployed to:", poseidonT3Address)
+        // Creating poseidon factories.
+        const poseidonT3Factory = new ethers.ContractFactory(generateABI(2), createCode(2), signer);
+        const poseidonT4Factory = new ethers.ContractFactory(generateABI(3), createCode(3), signer);
 
-    // Deploying PoseidonT4 library.
-    const poseidonT4 = await poseidonT4Factory.deploy()
-    await poseidonT4.deployed()
-    const poseidonT4Address = poseidonT4.address
-    console.log("PoseidonT4 deployed to:", poseidonT4Address)
+        // Deploying poseidon libraries.
+        const poseidonT3Address = await deployWithOptions("PoseidonT3", poseidonT3Factory, gasPrice, gasLimit);
+        const poseidonT4Address = await deployWithOptions("PoseidonT4", poseidonT4Factory, gasPrice, gasLimit);
 
-    // Setting .env variables.
-    parsedENVFile.POSEIDON_T3 = poseidonT3Address
-    parsedENVFile.POSEIDON_T4 = poseidonT4Address
-    fs.writeFileSync('.env', envfile.stringify(parsedENVFile))
+        // Setting environment variables.
+        parsedENVFile.POSEIDON_T3_ADDRESS = poseidonT3Address;
+        parsedENVFile.POSEIDON_T4_ADDRESS = poseidonT4Address;
+        fs.writeFileSync(".env", envfile.stringify(parsedENVFile));
 
-    // Exiting.
-    process.exit()
-}
+        // Logging success statement.
+        console.log("Completed deployment");
+    } catch (error) {
+        // Logging error statement.
+        console.error("Failed deployment");
 
-main().catch((error) => {
-    console.error(error)
-    process.exitCode = 1
-    process.exit()
-})
+        // Logging error data.
+        if (error?.transaction) {
+            console.error("Reason           :", error.reason);
+            console.error("Method           :", error.method);
+            console.error("Transaction from :", error.transaction.from);
+            console.error("Transaction to   :", error.transaction.to);
+            console.error("Transaction data :", error.transaction.data);
+        } else {
+            console.error(error);
+        }
+    }
+};
+
+const deployWithOptions = async (contractName, contractFactory, gasPrice, gasLimit) => {
+    const contract = await contractFactory.deploy({ gasPrice: gasPrice, gasLimit: gasLimit });
+    await contract.deployed();
+
+    const contractAddress = contract.address;
+
+    console.log(`${contractName} deployed to:`, contractAddress);
+
+    return contractAddress;
+};
+
+main();
